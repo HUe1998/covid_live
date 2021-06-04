7e82728d0a6992d8b126126edd018b2f94799b83
HUe1998 <himanshu.1998me@gmail.com>
2021-06-05 02:31:26 +0530

    Complete project. TODO: Deploy on website

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const ejs = require('ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
